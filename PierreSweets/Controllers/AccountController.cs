8197518d29661d79282abccd84d00e6d7c1c8eb2
Jeff Kim <kim2jy@hotmail.com>
2020-08-14 11:42:41 -0700

    Add Account Controller

---
+++ PierreSweets/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using PierreSweets.Models;
+using System.Threading.Tasks;
+using PierreSweets.ViewModels;
+
+namespace PierreSweets.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly PierreSweetsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+  }
+}
--- PierreSweets/Controllers/FlavorsController.cs
+++ PierreSweets/Controllers/FlavorsController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+using PierreSweets.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
