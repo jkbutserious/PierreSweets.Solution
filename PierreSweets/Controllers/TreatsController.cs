5580bcb5014fa52a1432868a34f22ae348ab10f6
Jeff Kim <kim2jy@hotmail.com>
2020-08-14 10:32:26 -0700

    Add Treats controller

---
+++ PierreSweets/Controllers/TreatsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using PierreSweets.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+//new using directives
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierreSweets.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly PierreSweetsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager; //new line
+
+    public TreatsController(UserManager<ApplicationUser> userManager, PierreSweetsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+  }
+}
