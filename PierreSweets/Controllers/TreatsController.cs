445bfea2c7f54cbc9f0347d38f1cb57684a91f40
Jeff Kim <kim2jy@hotmail.com>
2020-08-14 10:44:21 -0700

    Add Treats Create GET and POST controller methods

--- PierreSweets/Controllers/TreatsController.cs
+++ PierreSweets/Controllers/TreatsController.cs
@@ -24,12 +24,33 @@ namespace PierreSweets.Controllers
       _db = db;
     }
 
-     public async Task<ActionResult> Index()
+    public async Task<ActionResult> Index()
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
       var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id);
       return View(userTreats);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat, int CategoryId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
