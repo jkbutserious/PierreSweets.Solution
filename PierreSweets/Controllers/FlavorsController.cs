6b3e908fd42993ccd96df91b9f23c124e55fe480
Jeff Kim <kim2jy@hotmail.com>
2020-08-14 11:26:59 -0700

    Add Flavors controller

---
+++ PierreSweets/Controllers/FlavorsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
+  }
+}
